14b3930b0f3982611a1c8134aa0436efcefcb41a
bryanwalton <bryanwalton1997@gmail.com>
2022-04-07 11:09:37 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#include <stdio.h>
+include <stdio.h>
 #include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
