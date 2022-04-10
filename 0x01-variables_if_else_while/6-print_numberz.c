2fd4c8457ec252b15594e1c3ceb0fe3506023847
bryanwalton <bryanwalton1997@gmail.com>
2022-04-09 23:06:31 -0700

    r

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: Prints all single digit numbers of base 10 starting from 0 to 9
+  * Return: 0
+  */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; ++b)
+		putchar('0' + b);
+	putchar('\n');
+	return (0);
+}
