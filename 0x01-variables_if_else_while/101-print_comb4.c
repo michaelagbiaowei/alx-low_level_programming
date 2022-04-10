3e8374b5d6664a57dddae572e7c47e176fe2aab7
bryanwalton <bryanwalton1997@gmail.com>
2022-04-10 00:55:24 -0700

    f

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  *
@@ -9,8 +10,7 @@ int main(void)
 {
 	int c;
 	int d;
-	int e = 0;
-	
+	int e = 0;	
 	while (e < 10)
 	{
 		d = 0;
